ad9a16981ff9504dc13d18c1bce2fade25b376e5
razaasif <aasifraza9123@gmail.com>
2023-06-19 08:02:36 +0530

    created data config

---
+++ src/main/java/com/spj/config/DataSourceConfig.java
@@ -0,0 +1,20 @@
+package com.spj.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+//    @Bean
+//    public DataSource dataSource(){
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+//        dataSource.setUrl("jdbc:mysql://localhost:3306/jpa_test?createDatabaseIfNotExist=true");
+//        dataSource.setUsername("root");
+//        dataSource.setPassword("root");
+//        return dataSource;
+//    }
+}
